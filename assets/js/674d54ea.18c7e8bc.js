be4e527cc5e8a1789a0c33b6b46d2ea6e7c8a734
amirmohammd.ahmadi <amirmohammad.a@smartech.ir>
2025-05-28 23:07:29 +0330

    Deploy website - based on 3567d267054d731e151b6211a89949bb571eca5e

---
+++ assets/js/674d54ea.18c7e8bc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[531],{2658:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Publisher_sdk/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.42a93ca9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/Publisher_sdk/blog","blogTitle":"Blog","authorsListPath":"/Publisher_sdk/blog/authors"}')}}]);
